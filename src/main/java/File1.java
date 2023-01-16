16d7cccf86d5af86b8d78302df74fc770898b9a6
Aleksandr Blyudenov <abludenov@gmail.com>
2023-01-16 09:30:27 +0300

    Revert "add strings"
    This reverts commit 2f38be53e999c6bded9c4c6974a857d9057c51ae.

--- src/main/java/File1.java
+++ src/main/java/File1.java
@@ -2,10 +2,6 @@ public class File1 {
     private final String string = "Long from User1 from branch master";
     private final String string1 = "Some new feature456";
     private final String string3 = "lesson about rebase";
-    private final String string4 = "lesson about rebase";
-<<<<<<< HEAD
     private final String string5 = "lesson about rebase";
     private final String string6 = "lesson about rebase";
-=======
->>>>>>> 50ca8237731d06b724795b3c0dd590eb4b2dffef
 }
