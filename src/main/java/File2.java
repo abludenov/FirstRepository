320295d12093f81aebb551f318300cf643f0fc60
Aleksandr Blyudenov <abludenov@gmail.com>
2023-01-16 08:58:19 +0300

    add changes from user2

--- src/main/java/File2.java
+++ src/main/java/File2.java
@@ -3,4 +3,6 @@ public class File2 {
     private final String string2 = "String from User2";
     private final String string3 = "Line from User2";
     private final String string4 = "Fix critical bug";
+    private final String string5 = "line from user2";
+    private final String string6 = "lesson about rebase";
 }
