2f38be53e999c6bded9c4c6974a857d9057c51ae
Aleksandr Blyudenov <abludenov@gmail.com>
2023-01-16 09:25:34 +0300

    add strings

--- src/main/java/File1.java
+++ src/main/java/File1.java
@@ -3,4 +3,7 @@ public class File1 {
     private final String string1 = "Some new feature456";
     private final String string2 = "line from user1";
     private final String string3 = "lesson about rebase";
+    private final String string4 = "lesson about rebase";
+    private final String string5 = "lesson about rebase";
+    private final String string6 = "lesson about rebase";
 }
