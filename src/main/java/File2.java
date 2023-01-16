1b9f4d98ba13835366a1b7caef13b095f8a506bb
Aleksandr Blyudenov <abludenov@gmail.com>
2023-01-16 08:49:44 +0300

    fix critical bug

--- src/main/java/File2.java
+++ src/main/java/File2.java
@@ -2,4 +2,5 @@ public class File2 {
     private final String string1 = "String from User2";
     private final String string2 = "String from User2";
     private final String string3 = "Line from User2";
+    private final String string4 = "Fix critical bug";
 }
