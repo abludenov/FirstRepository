79cf938e0490f3d191be980f0f5b4a9e4a229282
Aleksandr Blyudenov <abludenov@gmail.com>
2023-01-16 09:03:42 +0300

    add 123 to some new feature

--- src/main/java/File1.java
+++ src/main/java/File1.java
@@ -1,6 +1,6 @@
 public class File1 {
     private final String string = "Long from User1 from branch master";
-    private final String string1 = "Some new feature";
+    private final String string1 = "Some new feature123";
     private final String string2 = "line from user1";
     private final String string3 = "lesson about rebase";
 }
